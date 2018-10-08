d5ce28456fba4950f3ca24e5588d9f98609d866b
smokezp <smoke111@email.ru>
2018-10-08 11:21:50 +0300

    replace blade to spa

---
+++ resources/js/components/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>
+            <router-link :to="{ name: 'product' }">Products</router-link>
+        </p>
+
+        <div class="container">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
