ea3c8838e8984b6836c10d4a1c48d925be3be949
smokezp <smoke111@email.ru>
2018-10-09 17:28:36 +0300

    add ide helper_models

--- app/Product.php
+++ app/Product.php
@@ -5,6 +5,24 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Product
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $name
+ * @property int $size
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Product whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Product whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Product whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Product whereSize($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Product whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Product whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Product extends Model
 {
     /**
